bdf280bb3b178358cd5b8fafa52b03a07f8d7550
Aleksey Moreev <emptybastard@mail.ru>
2019-03-30 05:36:25 +0500

    Use proper grammar

--- main.c
+++ main.c
@@ -9,6 +9,6 @@ printf("Enter your name: ");
 fgets(name, 255, stdin);
 name[strlen(name)-1] = '\0'; /* remove the newline at the end */
 
-printf("Hello %s!\n", name);
+printf("Hello, %s!\n", name);
 return 0;
 }
